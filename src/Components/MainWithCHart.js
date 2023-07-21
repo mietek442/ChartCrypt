da078c909a2515bc771f997964aee6afb5ae4857
mietek442 <adamzzz569@gmail.com>
2023-07-21 22:01:40 +0200

    showthechangeslikemainandsettingspanel

---
+++ src/Components/MainWithCHart.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./MainWithCHart.css";
+import Chart from "./Chart";
+
+function MainWithCHart() {
+  return (
+    <div className="MainWithPanel" id="adam">
+      <Chart />
+    </div>
+  );
+}
+
+export default MainWithCHart;
